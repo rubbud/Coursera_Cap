319c9828a19cc3039c9df6faa13e0ad141b26668
Rubin Buddhacharya <rubin_buddhacharya@hotmail.com>
2021-07-07 21:44:19 -0400

    Initial commit

---
+++ Capstone Project.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
